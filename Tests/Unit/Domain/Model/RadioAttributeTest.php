d0e864bb385d454a3b096ec2dde186db6063a9d0
Marcel Wieser <typo3dev@marcel-wieser.de>
2016-02-03 15:51:39 +0100

    [WIP][TASK] Reformats code to PSR-2

--- Tests/Unit/Domain/Model/RadioAttributeTest.php
+++ Tests/Unit/Domain/Model/RadioAttributeTest.php
@@ -2,29 +2,29 @@
 
 namespace Shop\Cadabra\Tests\Unit\Domain\Model;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2015 Marcel Wieser <typo3dev@marcel-wieser.de>
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+    /***************************************************************
+     *  Copyright notice
+     *
+     *  (c) 2015 Marcel Wieser <typo3dev@marcel-wieser.de>
+     *
+     *  All rights reserved
+     *
+     *  This script is part of the TYPO3 project. The TYPO3 project is
+     *  free software; you can redistribute it and/or modify
+     *  it under the terms of the GNU General Public License as published by
+     *  the Free Software Foundation; either version 2 of the License, or
+     *  (at your option) any later version.
+     *
+     *  The GNU General Public License can be found at
+     *  http://www.gnu.org/copyleft/gpl.html.
+     *
+     *  This script is distributed in the hope that it will be useful,
+     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *  GNU General Public License for more details.
+     *
+     *  This copyright notice MUST APPEAR in all copies of the script!
+     ***************************************************************/
 
 /**
  * Test case for class \Shop\Cadabra\Domain\Model\RadioAttribute.
@@ -36,26 +36,26 @@ namespace Shop\Cadabra\Tests\Unit\Domain\Model;
  */
 class RadioAttributeTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
 {
-	/**
-	 * @var \Shop\Cadabra\Domain\Model\RadioAttribute
-	 */
-	protected $subject = NULL;
+    /**
+     * @var \Shop\Cadabra\Domain\Model\RadioAttribute
+     */
+    protected $subject = null;
 
-	public function setUp()
-	{
-		$this->subject = new \Shop\Cadabra\Domain\Model\RadioAttribute();
-	}
+    public function setUp()
+    {
+        $this->subject = new \Shop\Cadabra\Domain\Model\RadioAttribute();
+    }
 
-	public function tearDown()
-	{
-		unset($this->subject);
-	}
+    public function tearDown()
+    {
+        unset($this->subject);
+    }
 
-	/**
-	 * @test
-	 */
-	public function dummyTestToNotLeaveThisFileEmpty()
-	{
-		$this->markTestIncomplete();
-	}
+    /**
+     * @test
+     */
+    public function dummyTestToNotLeaveThisFileEmpty()
+    {
+        $this->markTestIncomplete();
+    }
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -11,24 +11,22 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Cadabra',
-	'description' => '',
-	'category' => 'plugin',
-	'author' => 'Marcel Wieser',
-	'author_email' => 'typo3dev@marcel-wieser.de',
-	'state' => 'alpha',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.6.0-7.6.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'Cadabra',
+    'description' => '',
+    'category' => 'plugin',
+    'author' => 'Marcel Wieser',
+    'author_email' => 'typo3dev@marcel-wieser.de',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-7.6.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
 );
